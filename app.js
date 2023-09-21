676b955c792c839862065daa89a578bcf2dd47f8
Alexis072002 <alexis.dezeque@outlook.com>
2023-09-21 16:07:43 +0200

    ajouts et alignements des éléments

--- app.js
+++ app.js
@@ -6,54 +6,83 @@ const music = new Audio('audio/lambo.mp3');
 const songs = [
     {
         id:'1',
-        songName:' Petit <div class="subtitles">D.Z.K</div>',
+        songName:` Petit
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/homeless.jpg"
     },
     {
         id:'2',
-        songName:'Razzia <br> <div class="subtitles">Svnka ft. D.Z.K</div>',
+        songName:`Razzia <br> 
+        <div class="subtitles">Svnka ft. D.Z.K</div>`,
         poster: "img/lambo.jpg"
     },
     // all object type
     {
         id:'3',
-        songName:'Never give up <div class="subtitles">D.Z.K</div>',
+        songName:`Never give up 
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/nevergiveup.jpg"
     },
     {
         id:'4',
-        songName: ' Night Ft.SVNKA <div class="subtitles">D.Z.K</div>',
+        songName: `Night Ft.SVNKA 
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/night.jpg"
     },
     {
         id:'5',
-        songName: 'Broski <div class="subtitles">D.Z.K</div>',
+        songName: `Broski 
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/broski.jpg"
     },
     {
         id:'6',
-        songName:' Tripes Ft.SVNKA <div class="subtitles">D.Z.K</div>',
+        songName:` Tripes Ft.SVNKA 
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/tripes.jpg"
     },
     {
         id:'7',
-        songName:' c est donc ça nos vies <div class="subtitles">D.Z.K</div>',
+        songName:` c est donc ça nos vies 
+        <div class="subtitles">D.Z.K</div>`,
         poster: "img/nosvies.jpg"
     }
    
 ]
+
+
 Array.from(document.getElementsByClassName('songItem')).forEach((element, i)=>{
     element.getElementsByTagName('img')[0].src = songs[i].poster;
-    element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+    (element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName);
+    (document.getElementsByClassName('songItem'));
+
 })
 
 
 let masterPlay = document.getElementById('masterPlay');
+let wave = document.getElementsByClassName('wave')[0];
 
 masterPlay.addEventListener('click',()=>{
     if (music.paused || music.currentTime <=0) {
         music.play();
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+        wave.classList.add('active2');
     } else {
         music.pause();
+        masterPlay.classList.add('bi-play-fill');
+        masterPlay.classList.remove('bi-pause-fill');
+        wave.classList.remove('active2');
     }
+})
+
+
+let index =0;
+
+Array.from(document.getElementsByClassName('playListPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        index = e.target.id;
+        e.target.classList.remove('bi-play-circle-fill');
+        e.target.classList.add('bi-play-circle-fill');
+    })
 })
